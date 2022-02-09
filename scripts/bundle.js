c09f0917b8fa3cfdfdbe2cdcffc7b812e3193244
Catherine Jarocki <cjarocki@gmail.com>
2022-02-08 20:48:45 -0600

    update bundle to include logging and import

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -1,12 +1,15 @@
-const sh = require('child_process').execSync;
-const author = sh('git config --get user.email').toString().trim();
-const commits = sh(`git log --author=${author}`).toString().trim();
+import * as child from 'child_process';
+const author = child.execSync('git config --get user.email').toString().trim();
+const commits = child.execSync(`git log --author=${author}`).toString().trim();
+console.log("Commits by", author);
+console.log(commits);
 
 if (!commits) {
   console.error('This repo has no commits from the current git user. Did you commit your changes?');
   process.exit(1);
 }
 
-const result = sh(`git bundle create take-home-challenge.bundle HEAD main`);
-console.log(`Nice work, you created take-home-challenge.bundle.`);
+const bundleName = "take-home-challenge";
+const result = child.execSync(`git bundle create ${bundleName}.bundle HEAD main`);
+console.log(`Nice work, you created ${result}.`);
 console.log('Please upload it using the link sent to you in the initial email.');
